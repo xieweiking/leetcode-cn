5af6b49b1761d297602d06db956d1ddcb63a9f4f
生猛哥 <xieweiking@gmail.com>
2020-01-07 00:00:20 +0800

    rename var

--- problems/0004_median-of-two-sorted-arrays/binary-search/Solution.rs
+++ problems/0004_median-of-two-sorted-arrays/binary-search/Solution.rs
@@ -10,33 +10,33 @@ impl Solution {
         let (m, n) = (A.len(), B.len());
         let total = m + n;
         let (h, is_odd) = (total / 2, total % 2 == 1);
-        let (mut iBegin, mut iEnd) = (0, m);
-        while iBegin <= iEnd {
-            let i = (iBegin + iEnd) / 2;
+        let (mut i_begin, mut i_end) = (0, m);
+        while i_begin <= i_end {
+            let i = (i_begin + i_end) / 2;
             let j = h - i;
-            if i < iEnd && 0 < j && A[i] < B[j - 1] {
-                iBegin = i + 1;
-            } else if iBegin < i && j < n && B[j] < A[i - 1] {
-                iEnd = i - 1;
+            if i < i_end && 0 < j && A[i] < B[j - 1] {
+                i_begin = i + 1;
+            } else if i_begin < i && j < n && B[j] < A[i - 1] {
+                i_end = i - 1;
             } else {
-                let minGreater = if i == m {
-                                     B[j]
-                                 } else if j == n {
-                                     A[i]
-                                 } else {
-                                     cmp::min(A[i], B[j])
-                                 };
+                let min_greater = if i == m {
+                                      B[j]
+                                  } else if j == n {
+                                      A[i]
+                                  } else {
+                                      cmp::min(A[i], B[j])
+                                  };
                 if is_odd {
-                    return minGreater as f64;
+                    return min_greater as f64;
                 }
-                let maxLesser = if i == 0 {
-                                    B[j - 1]
-                                } else if j == 0 {
-                                    A[i - 1]
-                                } else {
-                                    cmp::max(A[i - 1], B[j - 1])
-                                };
-                return (maxLesser + minGreater) as f64 / 2.0;
+                let max_lesser = if i == 0 {
+                                     B[j - 1]
+                                 } else if j == 0 {
+                                     A[i - 1]
+                                 } else {
+                                     cmp::max(A[i - 1], B[j - 1])
+                                 };
+                return (max_lesser + min_greater) as f64 / 2.0;
             }
         }
         panic!("Should NOT be HERE!");
--- problems/0004_median-of-two-sorted-arrays/binary-search/solution.rb
+++ problems/0004_median-of-two-sorted-arrays/binary-search/solution.rb
@@ -11,30 +11,30 @@ def find_median_sorted_arrays(nums1, nums2)
     m, n = [a.length, b.length]
     total = m + n
     h = total / 2
-    isOdd = (total % 2 == 1)
-    iBegin, iEnd = [0, m]
-    while iBegin <= iEnd
-        i = (iBegin + iEnd) / 2
+    is_odd = (total % 2 == 1)
+    i_begin, i_end = [0, m]
+    while i_begin <= i_end
+        i = (i_begin + i_end) / 2
         j = h - i
-        if i < iEnd and 0 < j and a[i] < b[j - 1]
-            iBegin = i + 1
-        elsif iBegin < i and j < n and b[j] < a[i - 1]
-            iEnd = i - 1
+        if i < i_end and 0 < j and a[i] < b[j - 1]
+            i_begin = i + 1
+        elsif i_begin < i and j < n and b[j] < a[i - 1]
+            i_end = i - 1
         else
-            minGreater = (i == m ?
+            min_greater = (i == m ?
                     b[j] :
                     (j == n ?
                         a[i] :
                         [a[i], b[j]].min))
-            if isOdd
-                return minGreater
+            if is_odd
+                return min_greater
             end
-            maxLesser = (i == 0 ?
+            max_lesser = (i == 0 ?
                     b[j - 1] :
                     (j == 0 ?
                         a[i - 1] :
                         [a[i - 1], b[j - 1]].max))
-            return (maxLesser + minGreater) / 2.0
+            return (max_lesser + min_greater) / 2.0
         end
     end
     raise 'Should NOT be HERE!'
