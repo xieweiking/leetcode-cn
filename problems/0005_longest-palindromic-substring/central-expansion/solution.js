f633a00b8c49dcd93dc2fffe9235c0666346adc4
生猛哥 <xieweiking@gmail.com>
2020-01-07 12:04:59 +0800

    rename var

--- problems/0005_longest-palindromic-substring/central-expansion/solution.js
+++ problems/0005_longest-palindromic-substring/central-expansion/solution.js
@@ -4,15 +4,18 @@
  */
 var longestPalindrome = function (s) {
     'use strict'
-    const end = s.length * 2 - 1
-    let result = [0, 0]
-    for (let i = 0; i < end; ++i) {
-        let x = i >> 1,
-            y = (i % 2 === 1 ? x + 1 : x)
-        for (; x >= 0 && y < s.length && s[x] === s[y]; --x, ++y);
-        ++x
-        if (y - x > result[1] - result[0])
-            result = [x, y]
+    const iEnd = s.length * 2 - 1
+    let [maxLeft, maxRight] = [0, 0]
+    for (let i = 0; i < iEnd; ++i) {
+        let posLeft = i >> 1,
+            posRight = (i % 2 === 1 ? posLeft + 1 : posLeft)
+        while (posLeft >= 0 && posRight < s.length &&
+                s[posLeft] === s[posRight]) {
+            --posLeft
+            ++posRight
+        }
+        if (posRight - (++posLeft) > maxRight - maxLeft)
+            [maxLeft, maxRight] = [posLeft, posRight]
     }
-    return s.substring(...result)
+    return s.substring(maxLeft, maxRight)
 };
