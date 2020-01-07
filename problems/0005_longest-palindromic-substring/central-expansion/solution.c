42a0c3bb92b33793c016fbfb9a79f67ee6ce82b9
生猛哥 <xieweiking@gmail.com>
2020-01-07 13:27:35 +0800

    c solution

--- problems/0005_longest-palindromic-substring/central-expansion/solution.c
+++ problems/0005_longest-palindromic-substring/central-expansion/solution.c
@@ -1,7 +1,7 @@
 #define SUB_STR_PTR(s, start, len) ({                \
     char* __ss__ = malloc((len + 1) * sizeof(char)); \
     memcpy(__ss__, &s[start], len);                  \
-    __ss__[maxLen] = '\0';                           \
+    __ss__[len] = '\0';                              \
     __ss__;                                          \
 })
 
--- snippet/C/string.h
+++ snippet/C/string.h
@@ -1,6 +1,13 @@
 #define SUB_STR_PTR(s, start, len) ({                \
     char* __ss__ = malloc((len + 1) * sizeof(char)); \
     memcpy(__ss__, &s[start], len);                  \
-    __ss__[maxLen] = '\0';                           \
+    __ss__[len] = '\0';                           \
     __ss__;                                          \
+})
+
+#define SUB_STR_ARY(s, start, len) ({ \
+    char __ss__[len + 1];             \
+    memcpy(__ss__, &s[start], len);   \
+    __ss__[len] = '\0';            \
+    __ss__;                           \
 })
