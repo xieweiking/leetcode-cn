6b309cc146db19d4f44c8ad37960b7e9c00020d7
生猛哥 <xieweiking@gmail.com>
2020-01-09 14:04:16 +0800

    javascript solution

--- problems/0006_zigzag-conversion/solution.js
+++ problems/0006_zigzag-conversion/solution.js
@@ -8,8 +8,8 @@ var convert = function (s, numRows) {
     if (s.length <= 2 || numRows === 1)
         return s
     const colsPerPad = numRows - 1,
-        charsPerPad = colsPerPad - 1 + numRows,
-        lastRow = colsPerPad
+          charsPerPad = colsPerPad - 1 + numRows,
+          lastRow = colsPerPad
     let result = ''
     for (let r = 0; r < numRows; ++r)
         for (let i = r; i < s.length; i += charsPerPad) {
