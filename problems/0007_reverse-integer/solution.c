f277a7b299d696e98b4636670f337ac32ae9e695
生猛哥 <xieweiking@gmail.com>
2020-01-09 18:41:13 +0800

    refactor

--- problems/0007_reverse-integer/solution.c
+++ problems/0007_reverse-integer/solution.c
@@ -1,10 +1,7 @@
 int reverse(int x){
-    const int sign = (x < 0 ? -1 : 1);
-    long n = ((long) x) * sign,
-         result = 0;
-    for (; n != 0; n /= 10)
-        result = result * 10 + (n % 10);
-    result *= sign;
+    long result = 0;
+    for (; x != 0; x /= 10)
+        result = result * 10 + (x % 10);
     return (result < INT_MIN || INT_MAX < result ?
            0 : (int) result);
 }
--- problems/0007_reverse-integer/solution.js
+++ problems/0007_reverse-integer/solution.js
@@ -4,17 +4,9 @@
  */
 var reverse = function (x) {
     'use strict'
-    const sign = Math.sign(x)
-    x *= sign
     let result = 0
-    while (x != 0) {
-        const r = x % 10
-        result = result * 10 + r
-        x = (x - r) / 10
-    }
-    result *= sign
-    if (result < -2147483648 || 2147483647 < result)
-        return 0
-    else
-        return result
+    for (; x != 0; x = parseInt(x / 10))
+        result = result * 10 + (x % 10)
+    return (result < -2147483648 || 2147483647 < result ?
+            0 : result);
 };
