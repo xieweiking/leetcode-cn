40278732632c9e99eef9611b90061ad658c3e50e
生猛哥 <xieweiking@gmail.com>
2020-01-09 15:17:31 +0800

    ruby solution

--- problems/0006_zigzag-conversion/solution.rb
+++ problems/0006_zigzag-conversion/solution.rb
@@ -10,15 +10,15 @@ def convert(s, num_rows)
     cols_per_pad = num_rows - 1
     chars_per_pad = cols_per_pad - 1 + num_rows
     last_row = cols_per_pad
-    buf = StringIO.new
+    result = ''
     for r in 0..(num_rows - 1)
         for i in (r..(s.length - 1)).step(chars_per_pad)
-            buf << s[i]
+            result << s[i]
             j = i + chars_per_pad - 2 * r
             if (r != 0 and r != last_row and j < s.length)
-                buf << s[j]
+                result << s[j]
             end
         end
     end
-    return buf.string
+    return result
 end
