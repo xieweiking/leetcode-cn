5f918b0de55dcf8b1ce6dd030ff76fc44253c40e
生猛哥 <xieweiking@gmail.com>
2020-01-09 14:38:54 +0800

    c solution

---
+++ problems/0006_zigzag-conversion/solution.c
@@ -0,0 +1,18 @@
+char * convert(char * s, int numRows){
+    const int sLen = strlen(s);
+    if (sLen <= 2 || numRows == 1)
+        return s;
+    const int colsPerPad = numRows - 1,
+              charsPerPad = colsPerPad - 1 + numRows,
+              lastRow = colsPerPad;
+    char* result = malloc((sLen + 1) * sizeof(char));
+    result[sLen] = '\0';
+    for (int r = 0, k = 0; r < numRows; ++r)
+        for (int i = r; i < sLen; i += charsPerPad) {
+            result[k++] = s[i];
+            const int j = i + charsPerPad - 2 * r;
+            if (r != 0 && r != lastRow && j < sLen)
+                result[k++] = s[j];
+        }
+    return result;
+}
