e60e53b56376dfbe6631e4dfcd3b29f18f9ad3a3
生猛哥 <xieweiking@gmail.com>
2020-01-09 15:44:22 +0800

    swift solution

---
+++ problems/0006_zigzag-conversion/Solution.swift
@@ -0,0 +1,23 @@
+class Solution {
+    func convert(_ s: String, _ numRows: Int) -> String {
+        let sLen = s.count
+        if sLen <= 2 || numRows == 1 {
+            return s
+        }
+        let (chars, colsPerPad) = (Array(s), numRows - 1)
+        let (charsPerPad, lastRow) = (colsPerPad - 1 + numRows, colsPerPad)
+        var (buf, k) = ([Character](repeating: " ", count: sLen), 0)
+        for r in 0 ..< numRows {
+            for i in stride(from: r, to: sLen, by: charsPerPad) {
+                buf[k] = chars[i]
+                k += 1
+                let j = i + charsPerPad - 2 * r
+                if r != 0 && r != lastRow && j < sLen {
+                    buf[k] = chars[j]
+                    k += 1
+                }
+            }
+        }
+        return String(buf)
+    }
+}
