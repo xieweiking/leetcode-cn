bfea451c11422ed213e34ec402b67d58606224b7
生猛哥 <xieweiking@gmail.com>
2020-01-10 18:52:07 +0800

    javascript solution

---
+++ problems/0008_string-to-integer-atoi/built-in/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} str
+ * @return {number}
+ */
+var myAtoi = function (str) {
+    'use strict'
+    const result = parseInt(str)
+    if (isNaN(result))
+        return 0
+    else if (result < INT_MIN)
+        return INT_MIN
+    else if (INT_MAX < result)
+        return INT_MAX
+    else
+        return result
+};
+
+const INT_MAX = Math.pow(2, 31) - 1,
+      INT_MIN = -Math.pow(2, 31)
