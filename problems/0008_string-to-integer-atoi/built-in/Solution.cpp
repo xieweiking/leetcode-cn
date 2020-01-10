80516f978b6a64dcfb54b7435a19ad0761369c1a
生猛哥 <xieweiking@gmail.com>
2020-01-10 20:27:05 +0800

    cpp solution

--- problems/0008_string-to-integer-atoi/built-in/Solution.cpp
+++ problems/0008_string-to-integer-atoi/built-in/Solution.cpp
@@ -4,16 +4,10 @@ class Solution {
 public:
     int myAtoi(string str) {
         try {
-            const long result = stol(str);
-            if (result < INT_MIN)
-                return INT_MIN;
-            else if (result > INT_MAX)
-                return INT_MAX;
-            else
-                return (int) result;
+            return stoi(str);
         } catch (invalid_argument& ignored) {
         } catch (out_of_range& ignored) {
-            for (char& c : str)
+            for (const char& c : str)
                 if (c == '-')
                     return INT_MIN;
                 else if (c == '+' || '0' <= c && c <= '9')
