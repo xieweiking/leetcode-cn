ecf6967ef04e9cf03e05083ef85db2e46112e83c
生猛哥 <xieweiking@gmail.com>
2020-01-10 21:10:04 +0800

    ruby solution

---
+++ problems/0008_string-to-integer-atoi/built-in/solution.rb
@@ -0,0 +1,22 @@
+# @param {String} str
+# @return {Integer}
+def my_atoi(str)
+    if match = str.match(/^\s*(([\-\+]?)(0*)(\d+)).*$/)
+        begin
+            _, s, z, n = match.captures
+            result = Integer(s + n)
+            if result < INT_MIN
+                return INT_MIN
+            elsif result > INT_MAX
+                return INT_MAX
+            else
+                return result
+            end
+        rescue ArgumentError => ignored
+        end
+    end
+    return 0;
+end
+
+INT_MAX = 2**31 - 1
+INT_MIN = -2**31
