8fa1c97b5d17fff49ccedaa7949b792f2af5b618
生猛哥 <xieweiking@gmail.com>
2020-01-12 16:19:16 +0800

    c solution

---
+++ problems/0009_palindrome-number/reversed-int/solution.c
@@ -0,0 +1,10 @@
+bool isPalindrome(int x){
+    if (x < 0)
+        return false;
+    else if (x < 10)
+        return true;
+    long reversed = 0;
+    for (auto k = x; k != 0; k /= 10)
+        reversed = reversed * 10 + (k % 10);
+    return x == reversed;
+}
