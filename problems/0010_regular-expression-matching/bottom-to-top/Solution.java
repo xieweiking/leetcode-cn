fd0763eac4b6a5a1d989e3ff9fd556696009617c
生猛哥 <xieweiking@gmail.com>
2020-01-14 13:20:52 +0800

    java solution

---
+++ problems/0010_regular-expression-matching/bottom-to-top/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        final int sLen = s.length(), pLen = p.length();
+        final boolean[][] memo = new boolean[sLen + 1][pLen + 1];
+        memo[sLen][pLen] = true;
+        for (int i = sLen; i >= 0; --i)
+            for (int j = pLen - 1; j >= 0; --j) {
+                final boolean matchChar = (i < sLen &&
+                        (p.charAt(j) == s.charAt(i) || p.charAt(j) == '.'));
+                memo[i][j] = (j < pLen - 1 && p.charAt(j + 1) == '*' ?
+                        (memo[i][j + 2] || matchChar && memo[i + 1][j]) :
+                        (matchChar && memo[i + 1][j + 1]));
+            }
+        return memo[0][0];
+    }
+}
