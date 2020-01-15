75f3287b3642a0f3bfa5f3ff66fabd761da7029f
生猛哥 <xieweiking@gmail.com>
2020-01-15 19:06:08 +0800

    kotlin solution

---
+++ problems/0010_regular-expression-matching/backtrace/Solution.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun isMatch(s: String, p: String): Boolean {
+        if (p.isEmpty()) return (s.isEmpty())
+        val p0 = p[0]
+        val matchP0 = (s.length > 0 &&
+                (p0 == s[0] || p0 == '.'))
+        if (p.length >= 2 && p[1] == '*')
+            return (isMatch(s, p.substring(2)) ||
+                    (matchP0 && isMatch(s.substring(1), p)))
+        else
+            return (matchP0 && isMatch(s.substring(1), p.substring(1)))
+    }
+}
