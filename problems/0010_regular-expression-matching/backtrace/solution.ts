6c79efd97c8c14a1b064b6990010fa529321c355
生猛哥 <xieweiking@gmail.com>
2020-06-09 15:53:37 +0800

    Create solution.ts

---
+++ problems/0010_regular-expression-matching/backtrace/solution.ts
@@ -0,0 +1,11 @@
+function isMatch(s: string, p: string): boolean {
+    if (p.length == 0)
+        return (s.length == 0)
+    const p0 = p[0],
+        matchP0 = (s.length > 0 && (p0 == s[0] || p0 == '.'))
+    if (p.length >= 2 && p[1] == '*')
+        return (isMatch(s, p.substring(2)) ||
+                (matchP0 && isMatch(s.substring(1), p)))
+    else
+        return (matchP0 && isMatch(s.substring(1), p.substring(1)))
+};
