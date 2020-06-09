74280c4025ff035ee686551ff9fe434c87bef4cc
生猛哥 <xieweiking@gmail.com>
2020-06-09 15:22:46 +0800

    Create solution.ts

---
+++ problems/0006_zigzag-conversion/solution.ts
@@ -0,0 +1,18 @@
+function convert(s: string, numRows: number): string {
+    if (s.length <= 2 || numRows === 1)
+        return s
+    const colsPerPad = numRows - 1,
+        charsPerPad = colsPerPad - 1 + numRows,
+        lastRow = colsPerPad
+    let result = ''
+    for (let r = 0; r < numRows; ++r)
+        for (let i = r; i < s.length; i += charsPerPad) {
+            result += s[i]
+            if (r != 0 && r != lastRow) {
+                const j = i + charsPerPad - 2 * r
+                if (j < s.length)
+                    result += s[j]
+            }
+        }
+    return result
+};
