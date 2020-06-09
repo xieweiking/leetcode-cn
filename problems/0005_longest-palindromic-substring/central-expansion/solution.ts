5760febd7575d4e85deb66183f2221e009f83026
生猛哥 <xieweiking@gmail.com>
2020-06-09 15:18:31 +0800

    Create solution.ts

---
+++ problems/0005_longest-palindromic-substring/central-expansion/solution.ts
@@ -0,0 +1,16 @@
+function longestPalindrome(s: string): string {
+    const iEnd = s.length * 2 - 1
+    let [maxLeft, maxRight] = [0, 0]
+    for (let i = 0; i < iEnd; ++i) {
+        let posLeft = i >> 1,
+            posRight = (i % 2 === 1 ? posLeft + 1 : posLeft)
+        while (0 <= posLeft && posRight < s.length &&
+                s[posLeft] === s[posRight]) {
+            --posLeft
+            ++posRight
+        }
+        if (posRight - (++posLeft) > maxRight - maxLeft)
+            [maxLeft, maxRight] = [posLeft, posRight]
+    }
+    return s.substring(maxLeft, maxRight)
+};
